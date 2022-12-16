3a5d72d4eb1b5125cd0a824832ba19395d9c9db5
Daniela Schuck <114201069+danielaschuck@users.noreply.github.com>
2022-12-16 11:22:37 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import random
+print("com quem voce vai casar?")
+print("jogo de sorte")
+
+#da play para ver com quem vc vai se casar
+
+quem=["Loiro","moreno","careca","cabeludo","rei","ladrao","policia","capitao"]
+
+onde=["Casa", "apartamento","Chiqueiro"," galinheiro","Igreja", "castelo","Barraca", "cemitério"]
+
+x=random.choice(quem)
+y=random.choice(onde)
+print(f'vai casar com o {x} no {y}')
+                                       
+print("se gostou deixa um like")
+print("+-+ +-+ '-' ")
